029efc8dc1f5c96c8d5526f65a7aa43a2bb87a2b
Akshat-Singh <akshatsingh.hs@gmail.com>
2020-02-11 18:13:44 +0530

    Added the events float int. Too stopwatch to 1 min instead of 45.6 seconds

--- secondaries.py
+++ secondaries.py
@@ -4,6 +4,6 @@ import time
 def calculations():
     startTime = time.time()
     number = 0
-    while time.time() - startTime <= 1.0:
+    while time.time() - startTime <= 1.00:
         number = number + 1
     return number
